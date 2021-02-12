bdcc9684a55b0adf71e63f9fbe9581d76548ee0f
Augustinas Sukys <augustinas.sukys@gmail.com>
2021-02-12 12:42:39 +0000

    added function and tests
    basic checks

--- src/TestPackage.jl
+++ src/TestPackage.jl
@@ -1,6 +1,7 @@
 module TestPackage
 
     # Write your package code here.
-    include("extra_file.jl")
+include("extra_file.jl")
+export(test_f)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "TestPackage.jl" begin
     # Write your tests here.
+    @test test_f(4,1) == 24
+    @test test_f(1, 3) == 15
 end
