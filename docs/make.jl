9753cba4279602be95d3f24220c8292c64f5ea4b
Augustinas Sukys <augustinas.sukys@gmail.com>
2021-02-13 00:51:32 +0000

    changing make.jl
    hoping this fixes it

--- docs/make.jl
+++ docs/make.jl
@@ -4,13 +4,13 @@ using Documenter
 makedocs(;
     modules=[TestPackage],
     authors="Augustinas Sukys",
-    repo="https://github.com/augustinas1/TestPackage.jl/blob/{commit}{path}#L{line}",
+    #repo="https://github.com/augustinas1/TestPackage.jl/blob/{commit}{path}#L{line}",
     sitename="TestPackage.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://augustinas1.github.io/TestPackage.jl",
-        assets=String[],
-    ),
+    #format=Documenter.HTML(;
+    #    prettyurls=get(ENV, "CI", "false") == "true",
+#        canonical="https://augustinas1.github.io/TestPackage.jl",
+    #    assets=String[],
+    #),
     pages=[
         "Home" => "index.md",
         "Examples" => "examples.md"
